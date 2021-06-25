244a3cc8a5a2bc532fba6e74976bd90dae608814
Xavier Wonderful <x.teamwonderful@gmail.com>
2021-06-25 11:12:38 -0500

    Getting 8 completed

---
+++ Python 8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.formaat(1,2, 3, 4))
+print(formatter.formaat("one", "two", "three", "four"))
+print(formatter.formaat(True, False, False, True))
+print(formatter.formaat(formatter, formatter, formatter, formatter))
+print(formatter.formaat(
+    "Try your",
+    "Own your text here",
+    "Maybe a poem",
+    "Or a song about fear"
+    ))

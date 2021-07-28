9a475660b1fc3d7866b6a372a0d4d2081ccc9a2d
D. Wonderful <ddwonderful@gmail.com>
2021-07-28 17:40:37 -0600

    Fixing exercise 20 :)

--- Python 20.py
+++ Python 20.py
@@ -5,29 +5,32 @@ script, input_file = argv
 def print_all(f):
     print(f.read())
 
-    def rewind_all(f):
-        f.seek(0)
+def rewind(f):
+    f.seek(0)
 
-        def print_a_line(line_count, f):
-            print(line_count, f.readline())
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
 
-            current_file = open(input_file)
+current_file = open(input_file)
 
-            print("First let's print the whole file:\n")
+print("First let's print the whole file:\n")
 
-            print_all(current_file)
+print_all(current_file)
 
-            print("Now let's rewind, kind of like tape")
+print("Now let's rewind, kind of like tape")
 
-            rewind(current_file)
+rewind(current_file)
 
-            print("Let's print three lines")
-            
-            current_line = 1
-            print_a_line(current_line, current_file)
+print("Let's print three lines")
 
-            current_line = current_line = 1
-            print_a_line(current_line, current_file)
+current_line = 1
+print_a_line(current_line, current_file)
 
-            current_line = current_line + 1
-            print_a_line(current_line, current_file)
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
